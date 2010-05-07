45f8032469075d86c4ca003c383fa2f3c4c48abc
James Turk <james.p.turk@gmail.com>
2010-05-06 23:46:49 -0400

    removing highcharts dependency

--- ostracker/templatetags/ostracker_tags.py
+++ ostracker/templatetags/ostracker_tags.py
@@ -3,12 +3,27 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
+def scaled_bar_chart(w, h, data):
+    template = '''<img src="http://chart.apis.google.com/chart?cht=bhs&chs=%(w)sx%(h)s&chd=t:%(vals)s&chco=%(color)s&chxr=0,0,%(max)s,1&chds=0,%(max)s&chxt=x,y&chxl=1:|%(labels)s">'''
+
+    variables = {
+        'w': w, 'h': h,
+        'color': '009900',
+        'vals': ','.join(str(x) for x in data.values()),
+        'labels': '|'.join(data.keys()),
+        'max': max(data.values())
+    }
+
+    return template % variables
+
+
+@register.simple_tag
 def render_proj_variable(proj_variable):
     template = '''%(last)s <img src="http://chart.apis.google.com/chart?chs=80x30&cht=ls&chco=000099&chf=bg,s,00000000&chls=1,0,0&chd=t:%(all)s&chds=0,%(max)s">'''
 
     variables = {
         'last': proj_variable[-1],
-        'all': ','.join([str(x) for x in proj_variable]),
+        'all': ','.join(str(x) for x in proj_variable),
         'max': max(proj_variable)+1
     }
 
